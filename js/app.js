6c71e48bd4d8c567d20a8e806cd085bf5a55db08
Efe Amadasun <efeamadasun@gmail.com>
2021-11-15 14:57:05 +0000

    changes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,31 @@
 function init() {
   let params = (new URL(document.location)).searchParams;
-  let id = params.get("id");
+  let id = params.get("id").trim();
   document.getElementById('customer_id').textContent = id;
   document.getElementById('customer_id_txt').value = id;
+  getStatus(id);
+}
+
+function getStatus(id) {
+  let url = 'https://api.sheety.co/6d79bab42ccda02d92c2cd4536c74ba8/ntanta/customer?filter[activated]=true&filter[custId]=' + id;
+  fetch(url)
+  .then((response) => response.json())
+  .then(json => {
+    if (json.customers.length !== 0) {
+      setStatus(true, json.customers[0].name, 'Valid');
+    } else {
+      setStatus(false, json.customers[0].name, 'No Entry');
+    }
+  })
+  .catch(err => {
+    setStatus(false, '', 'System Error')
+  });
+}
+
+function setStatus(isValid, customerName, validationMsg) {
+  document.getElementById('name').textContent = customerName;
+  document.getElementById('status').textContent = validationMsg;
+  document.getElementById('status').className = isValid ? 'valid' : 'invalid';
 }
 
 function copy() {
